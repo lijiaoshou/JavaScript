931e07abf5e744b1407cd7eeeaa6fbd69f7275eb
超 李 <lichao_bj@hotmail.com>
2016-03-20 17:16:09 +0800

    首次提交

---
+++ JavascriptConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JavascriptConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double d = 1.056;
+            int a = Convert.ToInt32(d);
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
